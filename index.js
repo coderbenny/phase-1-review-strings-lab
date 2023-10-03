00db6e98bbbe78e343a9c073393eebfc95df00b2
bennymathew <benny.mathew@student.moringaschool.com>
2023-10-03 08:18:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'John';
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = "Welcome, " + firstInitial + "!";
+
